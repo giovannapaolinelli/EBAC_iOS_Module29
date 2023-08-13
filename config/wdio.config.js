181e233e63e85a58810e760502795edf8788f0ab
Giovanna <giovanna.paolinelli@gmail.com>
2023-08-13 18:46:45 -0300

    setup wdio file

---
+++ config/wdio.config.js
@@ -0,0 +1,17 @@
+const{localConf} = require('./local.conf')
+
+require('dotenv').config()
+
+function getConfig(){
+    switch (process.env.ENVIRONMENT) {
+        case local: default:
+            return localConf    
+        case 'browserstack':
+            return {}
+        case 'saucelabs':
+            return {}
+    }
+}
+
+
+exports.config = {}
