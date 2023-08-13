8fc5fb85f3a4f8729bc9498102aff556b5fb3f22
Giovanna <giovanna.paolinelli@gmail.com>
2023-08-13 20:29:24 -0300

    config report

--- config/reports.conf.js
+++ config/reports.conf.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const allure = require('allure-commandline')
 const video = require('wdio-video-reporter')
 
-let reportsConf = process.env.REPORT == true ? {
+let reportsConf = process.env.REPORT == 'true' ? {
 
     reporters: ['spec',
         ['allure', {
